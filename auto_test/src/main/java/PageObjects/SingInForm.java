1cd256584cf905afc03ad9134eedbdaf330c497e
Sarvarov Roman <sarvarovrg@yandex.ru>
2022-08-08 21:32:04 +0300

    Sing_in

--- auto_test/src/main/java/PageObjects/SingInForm.java
+++ auto_test/src/main/java/PageObjects/SingInForm.java
@@ -1,4 +1,14 @@
 package PageObjects;
 
+import org.openqa.selenium.WebDriver;
+
 public class SingInForm {
+
+    WebDriver driver;
+
+    public SingInForm(WebDriver driver){
+        this.driver = driver;
+    }
+
+
 }
